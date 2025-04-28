40f475846344b8b017e90a75a0d9eb8d05b03304
ponvasanth <ponvasanth.rangasamy@isteer.com>
2025-04-28 15:15:21 +0530

    Added endpoints for getting all the vulnerabilities

--- CVSS-Analyzer/CVSS-Analyzer-Api-Starter/src/main/java/com/isteer/cvssanalyzer/api/controller/CvssController.java
+++ CVSS-Analyzer/CVSS-Analyzer-Api-Starter/src/main/java/com/isteer/cvssanalyzer/api/controller/CvssController.java
@@ -1,13 +1,10 @@
 package com.isteer.cvssanalyzer.api.controller;
 
-import java.util.List;
 import java.io.IOException;
-import java.util.ArrayList;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.isteer.cvssanalyzer.api.service.CvssService;
@@ -20,35 +17,19 @@ public class CvssController {
 	public CvssController(CvssService cvssService) {
 		this.service = cvssService;
 	}
-
-	@GetMapping("/")
-	public List<String> handshake1() {
-		List<String> list = new ArrayList<>();
-		list.add("Kavin");
-		return list;
-	}
-
-	@GetMapping("/hi")
-	public List<String> handshake() {
-		try {
-			return service.dummyValues();
-		} catch (IOException | InterruptedException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	public static void main(String[] args) {
-		CvssService service1 = new CvssService();
-		List<String> list = new ArrayList<>();
-		try {
-			list = service1.dummyValues();
-		} catch (IOException | InterruptedException e) {
+	
+	@GetMapping("/getVulnerabilities")
+	public ResponseEntity<Object> getAllVulnerabilities() {
+//		try {
+			return ResponseEntity.ok(service.getAllVulnerabilities());
+//		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		for (String s : list) {
-			System.out.println(s);
-		}
+//			e.printStackTrace();
+//			return ResponseEntity.status(500).body("Internal Server Error");
+//		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//			return ResponseEntity.status(500).body("Internal Server Error");
+//		}
 	}
 }
