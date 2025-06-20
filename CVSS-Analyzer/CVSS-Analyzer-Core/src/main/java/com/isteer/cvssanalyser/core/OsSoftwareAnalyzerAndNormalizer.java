1707e39cc807a4085e0a9e3d4a85ea5b59e321aa
ponvasanth <ponvasanth.rangasamy@isteer.com>
2025-06-20 18:01:26 +0530

    Updated the code for the recent changes made in the model and integrated cve client to find the vulnerabilities for the resolved cpe name of the application

--- CVSS-Analyzer/CVSS-Analyzer-Core/src/main/java/com/isteer/cvssanalyser/core/OsSoftwareAnalyzerAndNormalizer.java
+++ CVSS-Analyzer/CVSS-Analyzer-Core/src/main/java/com/isteer/cvssanalyser/core/OsSoftwareAnalyzerAndNormalizer.java
@@ -8,14 +8,17 @@ import java.util.List;
 import java.util.Set;
 
 import com.isteer.cvssanalyser.core.dao.DependencyHintDao;
+import com.isteer.cvssanalyser.core.enums.CpeField;
 import com.isteer.cvssanalyser.core.enums.EvidenceType;
+import com.isteer.cvssanalyser.core.enums.ResolveMethod;
 import com.isteer.cvssanalyser.core.model.ApplicationModel;
+import com.isteer.cvssanalyser.core.model.CPENameModel;
 import com.isteer.cvssanalyser.core.model.CpeEntryModel;
 import com.isteer.cvssanalyser.core.model.DependencyModel;
 import com.isteer.cvssanalyser.core.model.Evidence;
 import com.isteer.cvssanalyser.core.util.DbUtil;
 
-public class OsSoftwareAnalyzer {
+public class OsSoftwareAnalyzerAndNormalizer {
 
 	public List<DependencyModel> resolveOsSoftwareNames(ApplicationModel application) throws SQLException {
 		// This method will resolve the OS software names from the list of applications
@@ -25,9 +28,13 @@ public class OsSoftwareAnalyzer {
 		FuzzySearchTool fuzzySearchTool = new FuzzySearchTool();
 		List<DependencyModel> resolvedApplications = new ArrayList<>();
 		DependencyModel dependency = new DependencyModel();
+
 		Evidence productEvidence = new Evidence();
 		Evidence vendorEvidence = new Evidence();
 		Evidence versionEvidence = new Evidence();
+
+		CPENameModel cpe = new CPENameModel();
+
 		String evidenceTitle = "Application-Metadata";
 
 		vendorEvidence.setEvidenceType(EvidenceType.APPLICATION);
@@ -47,10 +54,12 @@ public class OsSoftwareAnalyzer {
 				application.getApplicationVendor());
 
 		if (resolvedVendor != null && !resolvedVendor.isEmpty()) {
+			vendorEvidence.setResolvedValue(resolvedVendor.iterator().next());
+			cpe.addResolveMethod(CpeField.VENDOR, ResolveMethod.HINT_BY_DEVELOPER);
+
 			List<CpeEntryModel> resolvedProduct = fuzzySearchTool.searchForLikelyProducts(resolvedVendor,
 					application.getApplicationName());
 			if (resolvedProduct != null && !resolvedProduct.isEmpty()) {
-				vendorEvidence.setResolvedValue(resolvedProduct.get(0).getVendor());
 				productEvidence.setResolvedValue(resolvedProduct.get(0).getProduct());
 			} else {
 				productEvidence.setResolvedValue(null);
@@ -67,11 +76,22 @@ public class OsSoftwareAnalyzer {
 				productEvidence.setResolvedValue(similarCpe.getProduct());
 			}
 		}
+		if (productEvidence.getResolvedValue() != null && vendorEvidence.getResolvedValue() != null
+				&& versionEvidence.getResolvedValue() != null) {
+			cpe.setProduct(productEvidence.getResolvedValue());
+			cpe.addResolveMethod(CpeField.PRODUCT, ResolveMethod.FUZZY_SEARCH);
+			cpe.setVendor(vendorEvidence.getResolvedValue());
+			cpe.setVersion(versionEvidence.getResolvedValue());
+			cpe.addResolveMethod(CpeField.VERSION, ResolveMethod.ARBITRARY);
+		}
+
 		dependency.addVendorEvidence(vendorEvidence);
 		dependency.addProductEvidences(productEvidence);
 		dependency.addVersionEvidences(versionEvidence);
+		dependency.setCpeEnumeration(cpe);
 
 		resolvedApplications.add(dependency);
+//		System.out.println(resolvedApplications);
 		return resolvedApplications;
 	}
 }
--- CVSS-Application-Datastore/src/main/java/com/isteer/controller/ComputerController.java
+++ CVSS-Application-Datastore/src/main/java/com/isteer/controller/ComputerController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.isteer.cvssanalyser.core.cache.FuzzySearchCache;
 import com.isteer.dto.ComputerDetailsResponseDTO;
 import com.isteer.dto.ComputerPayloadDTO;
 import com.isteer.dto.ErrorMessageDto;
@@ -41,6 +42,8 @@ public class ComputerController {
 
 	@PostMapping("/computers")
 	public ResponseEntity<?> createComputer(@Valid @RequestBody ComputerPayloadDTO payload) {
+		new FuzzySearchCache();
+		FuzzySearchCache.refreshCacheFromDb(); // Refresh cache before processing request
 		logger.info("Received request to create/update computer with deviceId: {}", payload.getDeviceId());
 		int status = computerService.createComputer(payload);
 
