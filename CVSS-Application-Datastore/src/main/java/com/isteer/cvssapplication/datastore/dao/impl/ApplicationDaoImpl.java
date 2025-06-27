e76475e105cf0be90ca3da5f368d1e495c95c60e
ponvasanth <ponvasanth.rangasamy@isteer.com>
2025-06-27 14:54:23 +0530

    Modified inserting vulnerability logic to avoid duplicate entries

--- CVSS-Application-Datastore/src/main/java/com/isteer/cvssapplication/datastore/dao/impl/ApplicationDaoImpl.java
+++ CVSS-Application-Datastore/src/main/java/com/isteer/cvssapplication/datastore/dao/impl/ApplicationDaoImpl.java
@@ -1,8 +1,6 @@
 package com.isteer.cvssapplication.datastore.dao.impl;
 
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
