6dea27cbecd8ac5e5e9586b7556083b71a6c78d7
kavin-isteer <kavinkr.isteer.com>
2025-06-30 10:33:32 +0530

    Added null check and skips refresh cache.

--- CVSS-Analyzer/CVSS-Analyzer-Core/src/main/java/com/isteer/cvssanalyser/core/cache/FuzzySearchCache.java
+++ CVSS-Analyzer/CVSS-Analyzer-Core/src/main/java/com/isteer/cvssanalyser/core/cache/FuzzySearchCache.java
@@ -33,7 +33,7 @@ public class FuzzySearchCache {
 
     // Optionally refresh manually or on a schedule
     public static void refreshCacheFromDb() {
-    	if(cachedCpeEntries.get().size()>0) {
+    	if(cachedCpeEntries.get()!=null && cachedCpeEntries.get().size()>0) {
     		return;
     	}
         List<CpeEntryModel> freshData=new ArrayList<>();
